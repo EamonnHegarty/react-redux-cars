50153432cc2efb1f86df29bba4534e45790b3424
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-01-04 12:58:09 +0000

    Added styling, form reset through redux, search & extra reducers

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,11 +1,17 @@
-import { useSelector, useDispatch } from "react-redux";
-import { removeCar } from "../store";
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
 
 function CarList() {
   const dispatch = useDispatch();
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-  const cars = useSelector((state) => {
-    return state.cars.data;
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -13,8 +19,11 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    // DECIDE IF THIS CAR SHOULD BE BOLD
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
